7cc3c8190e4055a14d6ebe60f951b6a4bbb38e51
dijie <dijie@mogu.com>
2020-05-29 16:08:26 +0800

    JSON&HBASE

--- src/main/java/FlinkSql/FlinkSql06.java
+++ src/main/java/FlinkSql/FlinkSql06.java
@@ -1,10 +1,11 @@
 package FlinkSql;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 
-import static util.FlinkConstant.env;
-import static util.FlinkConstant.tEnv;
 
 public class FlinkSql06 {
 
@@ -26,7 +27,7 @@ public class FlinkSql06 {
             "    'format.type' = 'json'  -- json格式，和topic中的消息格式保持一致\n" +
             ")";
 
-    public static final String HBASE_TABLE_DIM_DDL = ""+
+    public static final String HBASE_TABLE_DIM_DDL = "" +
             "CREATE TABLE t2 (\n" +
             "rowkey String,\n" +
             "f1 ROW<col1 String>\n" +
@@ -39,20 +40,26 @@ public class FlinkSql06 {
             ")";
 
     public static void main(String[] args) throws Exception {
-        //{"afterColumns":{"created":"1589186680","extra":{"canGiving":false},"parameter":[1,2,3,4]},"beforeColumns":null,"tableVersion":{"binlogFile":null,"binlogPosition":0,"version":0},"touchTime":1589186680591}
 
+        //构建StreamExecutionEnvironment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //构建EnvironmentSettings 并指定Blink Planner
+        EnvironmentSettings bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+
+        //构建StreamTableEnvironment
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env, bsSettings);
+
+        //执行DDL
         tEnv.sqlUpdate(KAFKA_TABLE_SOURCE_DDL_01);
 
         tEnv.sqlUpdate(HBASE_TABLE_DIM_DDL);
 
+        //注意FOR SYSTEM_TIME AS OF a.p AS b 的用法，左表需要用proctime，我在DDL指定了，大家记得看好
         Table table = tEnv.sqlQuery("select a.* ,b.* from t1 a left join  t2 FOR SYSTEM_TIME AS OF a.p AS b on a.afterColumns.created = b.rowkey");
 
-        Table table2 = tEnv.sqlQuery("select afterColumns.created from t1");
-
-        table.printSchema();
-
-        tEnv.toRetractStream(table, Row.class).print();
-        tEnv.toRetractStream(table2, Row.class).print();
+        //转成流输出
+        tEnv.toAppendStream(table, Row.class).print().setParallelism(1);
 
         env.execute("Flink Sql 06");
     }
